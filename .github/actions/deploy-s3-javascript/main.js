cfd3d1398c2675fa79867670a96e0795d418f991
chandra1591 <chandrabhan99@gmail.com>
2025-11-30 15:04:27 +0530

    test actions

--- .github/actions/deploy-s3-javascript/main.js
+++ .github/actions/deploy-s3-javascript/main.js
@@ -1,10 +1,9 @@
 const core = require('@actions/core');
-const github = require('@actions/github');
-const exec = require('@actions/exec');
+
 function run() {
     console.log("Deploying to S3 using JavaScript action...");
     core.notice("This is a placeholder for S3 deployment logic.");
-    
+
     // Add your deployment logic here
 }
 
