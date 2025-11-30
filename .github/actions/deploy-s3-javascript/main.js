736c86010f891d4e1406b750d342cf7807597cd0
chandra1591 <chandrabhan99@gmail.com>
2025-11-30 17:44:39 +0530

    add exec aws s3

--- .github/actions/deploy-s3-javascript/main.js
+++ .github/actions/deploy-s3-javascript/main.js
@@ -1,7 +1,30 @@
 const core = require('@actions/core');
+//const github = require('@actions/github');
+const exec = require('@actions/exec');
+
+
 
 function run() {
     console.log("Deploying to S3 using JavaScript action...");
+
+    // 1) Get some input values
+    // const bucket = core.getInput('bucket', { required: true });
+    // const bucketRegion = core.getInput('bucket-region', { required: true });
+    // const distFolder = core.getInput('dist-folder', { required: true });
+
+    // core.notice(`Bucket: ${bucket}`);
+    // core.notice(`Bucket Region: ${bucketRegion}`);
+    // core.notice(`Distribution Folder: ${distFolder}`);
+
+    
+
+    // // 2) Upload files
+    // const s3Uri = `s3://${bucket}/`;
+    // exec.exec('echo', ['Uploading files to S3...']);
+    // exec.exec(`aws s3 sync ${distFolder} ${s3Uri} --region ${bucketRegion}`);
+
+
+
     core.notice("This is a placeholder for S3 deployment logic.");
 
     // Add your deployment logic here
